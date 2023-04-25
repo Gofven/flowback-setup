11eb68280f61bfd439119328d377739ae8d03bbc
gofven <cratedcreations@gmail.com>
2023-04-25 03:22:30 +0200

    Added LICENSE.txt Added rabbitmq.bash

---
+++ resources/rabbitmq.bash
@@ -0,0 +1,7 @@
+docker run -d --hostname "$(hostname)" --name rabbitmq-flowback -p 5672:5672 --restart unless-stopped rabbitmq:latest
+sleep 5
+docker exec -ti rabbitmq-flowback bash -c "
+rabbitmqctl add_user flowback flowback ;
+rabbitmqctl add_vhost rabbitmq-flowback ;
+rabbitmqctl set_user_tags flowback administrator ;
+rabbitmqctl set_permissions -p rabbitmq-flowback flowback \".*\" \".*\" \".*\" ;"
